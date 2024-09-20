4f39d55f9adb7991be2af0ca66e0a6cdedad888b
root <root@ip-172-31-46-116.eu-west-3.compute.internal>
2024-09-20 07:04:30 +0000

    Initial commit of TypeScript demo project

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// src/index.ts
+const greet = (name) => {
+    return `Hello, ${name}! Welcome to the TypeScript demo project.`;
+};
+console.log(greet("Prathamesh"));
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// src/index.ts
+const greet = (name: string): string => {
+    return `Hello, ${name}! Welcome to the TypeScript demo project.`;
+};
+
+console.log(greet("Prathamesh"));
+
